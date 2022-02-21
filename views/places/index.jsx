fa6a512a27857dbd074bb1ef155a970ef206db8f
Chris Chicas <christianchicas7@gmail.com>
2022-02-21 15:13:48 -0500

    temp key fix

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,9 +2,9 @@ const React = require("react")
 const Def = require("../default.jsx")
 
 function Index(data){
-    let placesFormatted = data.places.map((place) => {
+    let placesFormatted = data.places.map((place, index) => {
         return (
-            <div className="col-sm-6 text-center" key={"0123"}>
+            <div className="col-sm-6 text-center" key={index}>
                 <h2>{place.name}</h2>
                 <p>
                     {place.cuisines}
