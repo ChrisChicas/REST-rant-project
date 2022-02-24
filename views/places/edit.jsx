a733e91136553d6383dd83f4b7f2f6b9e850d82d
Chris Chicas <christianchicas7@gmail.com>
2022-02-23 22:14:55 -0500

    Added the edit functionality

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -1,32 +1,32 @@
 const React = require("react")
 const Def = require("../default.jsx")
 
-function new_form(){
+function edit_form(data){
     return (
         <Def>
             <main>
-                <h1>Add a New Place</h1>
-                <form action="/places" method="POST">
+                <h1>Edit Place</h1>
+                <form action={`/places/${data.id}?_method=PUT`} method="POST">
                     <div className="row">
                         <div className="form-group col-sm-6">
                             <label htmlFor="name">Place Name</label>
-                            <input className="form-control" type="text" name="name" id="name" required/>   
+                            <input className="form-control" type="text" name="name" id="name" defaultValue={data.place.name} required/>   
                         </div>                    
                         <div className="form-group col-sm-6">
                                 <label htmlFor="pic">Place Picture</label>
-                                <input className="form-control" type="url" name="pic" id="pic" />   
+                                <input className="form-control" type="url" name="pic" id="pic" defaultValue={data.place.pic} />   
                         </div>                    
                         <div className="form-group col-sm-6">
                                 <label htmlFor="city">City</label>
-                                <input className="form-control" type="text" name="city" id="city" />    
+                                <input className="form-control" type="text" name="city" id="city" defaultValue={data.place.city}/>    
                         </div>
                         <div className="form-group col-sm-6">
                                 <label htmlFor="state">State</label>
-                                <input className="form-control" type="text" name="state" id="state" />    
+                                <input className="form-control" type="text" name="state" id="state" defaultValue={data.place.state} />    
                         </div>
                         <div className="form-group">
                                 <label htmlFor="cuisines">Cuisines</label>
-                                <input className="form-control" type="text" name="cuisines" id="cuisines" required/>    
+                                <input className="form-control" type="text" name="cuisines" id="cuisines" defaultValue={data.place.cuisines} required/>    
                         </div>  
                     </div>
                     <input className="btn btn-primary" type="submit" value="Add Place" />
@@ -36,4 +36,4 @@ function new_form(){
     )
 }
 
-module.exports = new_form
+module.exports = edit_form
