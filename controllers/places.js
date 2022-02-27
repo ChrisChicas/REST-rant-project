183d1f609554c6260d1cbe177b23a816099073d6
Chris Chicas <christianchicas7@gmail.com>
2022-02-27 15:07:22 -0500

    show page functional w/ mongoose

--- controllers/places.js
+++ controllers/places.js
@@ -17,14 +17,14 @@ router.get("/new", (req, res) => {
 })
 
 router.get("/:id", (req, res) => {
-    let id = Number(req.params.id)
-    if(isNaN(id)){
+    db.Place.findById(req.params.id)
+    .then(place => {
+        res.render("./places/show", {place})
+    })
+    .catch(err => {
+        console.log("err", err)
         res.render("error404")
-    }else if(!places[id]){
-        res.render("error404")
-    } else{
-        res.render("./places/show", {place : places[id], id})
-    }
+    })
 })
 
 router.post("/", (req, res) => {
--- views/places/index.jsx
+++ views/places/index.jsx
@@ -2,11 +2,11 @@ const React = require("react")
 const Def = require("../default.jsx")
 
 function Index(data){
-    let placesFormatted = data.places.map((place, index) => {
+    let placesFormatted = data.places.map((place) => {
         return (
-            <div className="col-sm-6" key={index}>
+            <div className="col-sm-6" key={place._id}>
                 <h2>
-                    <a href={`/places/${index}`}>
+                    <a href={`/places/${place._id}`}>
                         {place.name}
                     </a>
                 </h2>
