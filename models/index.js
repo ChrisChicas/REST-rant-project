2758aa3ee11b847dfdb24274e750fedfb55ce7a3
Chris Chicas <christianchicas7@gmail.com>
2022-03-04 01:18:34 -0500

    ran comment seeder

--- models/index.js
+++ models/index.js
@@ -3,3 +3,4 @@ require("dotenv").config()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 module.exports.Place = require("./places")
+module.exports.Comment = require("./comment")
